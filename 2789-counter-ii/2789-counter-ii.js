bf340f40ea770c3ed8fbc37e4bad4c173051b07b
Aby Shaji <abyshaji318@gmail.com>
2025-10-04 14:45:57 +0530

    Time: 58 ms (16.5%), Space: 56.1 MB (83.27%) - LeetHub

--- 2789-counter-ii/2789-counter-ii.js
+++ 2789-counter-ii/2789-counter-ii.js
@@ -2,12 +2,21 @@
  * @param {integer} init
  * @return { increment: Function, decrement: Function, reset: Function }
  */
-var createCounter = function(init) {
-    let counter=init
-    return{
-        increment:()=> ++counter,
-        decrement:()=> --counter,
-        reset:()=> counter=init
+var createCounter = function (init) {
+    let counter = init
+    return {
+        increment: function () {
+            counter += 1
+            return counter
+        },
+        decrement: function () {
+            counter -= 1
+            return counter
+        },
+        reset: function () {
+            counter = init
+            return counter
+        }
 
     }
 };
