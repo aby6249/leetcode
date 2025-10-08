0099623685a0de63f51391596262d4dc00f3a0ea
Aby Shaji <abyshaji318@gmail.com>
2025-10-08 15:08:26 +0530

    Time: 0 ms (100%), Space: 53.4 MB (80.66%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -2,14 +2,14 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
+var lengthOfLastWord = function (s) {
     let spell = 0
-    for(let i=s.length-1;i>=0;i--){
-        if(s[i] !== " "){
-            spell ++;
-            
-        }else{
-            if(spell !==0)break
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] !== " ") {
+            spell++;
+
+        } else {
+            if (spell !== 0) break
         }
     }
     return spell
