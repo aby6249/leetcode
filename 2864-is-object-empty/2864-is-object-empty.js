f3a3df5d34832674b9e0fb93b700f41fdbf26037
Aby Shaji <abyshaji318@gmail.com>
2025-10-14 10:58:19 +0530

    Time: 56 ms (6.05%), Space: 54.8 MB (46.87%) - LeetHub

--- 2864-is-object-empty/2864-is-object-empty.js
+++ 2864-is-object-empty/2864-is-object-empty.js
@@ -2,8 +2,8 @@
  * @param {Object|Array} obj
  * @return {boolean}
  */
-var isEmpty = function(obj) {
-    for(key in obj){
+var isEmpty = function (obj) {
+    for (key in obj) {
         return false
     }
     return true
