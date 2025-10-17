6495ef0e8a1405a7da8a7c41fcc566b998980a71
Aby Shaji <abyshaji318@gmail.com>
2025-10-17 10:19:33 +0530

    Time: 46 ms (48.75%), Space: 54.4 MB (24.37%) - LeetHub

--- 2746-filter-elements-from-array/2746-filter-elements-from-array.js
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -3,11 +3,11 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var filter = function(arr, fn) {
-    let num=[]
-    for(let i=0;i<arr.length;i++){
-        if(fn(arr[i],i)){
-               num.push(arr[i])
+var filter = function (arr, fn) {
+    let num = []
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            num.push(arr[i])
         }
     }
     return num
