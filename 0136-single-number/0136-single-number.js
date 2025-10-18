5d7edfa797c2a07fa2e6603025b8d67fad1efbd7
Aby Shaji <abyshaji318@gmail.com>
2025-10-18 10:03:32 +0530

    Time: 2 ms (57.83%), Space: 58 MB (33.97%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let grill=0
+    for (let number of nums){
+        grill ^=number
+
+    }
+    return grill
+};
