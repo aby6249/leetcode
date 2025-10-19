96f2a5f3958d28b9d0c4510c58ac86ca88853721
Aby Shaji <abyshaji318@gmail.com>
2025-10-19 22:03:05 +0530

    Time: 1 ms (34.49%), Space: 55.9 MB (45.47%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.js
+++ 0263-ugly-number/0263-ugly-number.js
@@ -2,12 +2,12 @@
  * @param {number} n
  * @return {boolean}
  */
-var isUgly = function(n) {
-    if(n<=0) return false
+var isUgly = function (n) {
+    if (n <= 0) return false
 
-    while(n%2==0)n/=2
-    while(n%3==0)n/=3
-    while(n%5==0)n/=5
+    while (n % 2 == 0) n /= 2
+    while (n % 3 == 0) n /= 3
+    while (n % 5 == 0) n /= 5
 
-    return n===1
+    return n === 1
 };
