eaf55d17e2018dfd627074843f9a4156f9ed581c
Aby Shaji <abyshaji318@gmail.com>
2025-10-21 11:56:03 +0530

    Time: 7 ms (88.36%), Space: 60.9 MB (42.86%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+        let x=s.split(" ").map(letter=>letter.split("").reverse().join(""))
+    return x.join(" ")
+};
