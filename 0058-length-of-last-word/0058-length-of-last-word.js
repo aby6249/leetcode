578aca8112705b781fbcdc670407e9d5ed70b677
Aby Shaji <abyshaji318@gmail.com>
2025-10-22 10:08:19 +0530

    Time: 0 ms (100%), Space: 54.7 MB (17.67%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,14 +3,6 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
-    let spell = 0
-    for (let i = s.length - 1; i >= 0; i--) {
-        if (s[i] !== " ") {
-            spell++;
-
-        } else {
-            if (spell !== 0) break
-        }
-    }
-    return spell
+     let x=s.trim().split(" ")
+    return x[x.length-1].length
 };
