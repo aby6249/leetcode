5b4b27c9ded7387c3d9679e4367b4e5c8ef4b8d0
Aby Shaji <abyshaji318@gmail.com>
2025-10-27 15:22:57 +0530

    Time: 60 ms (42.16%), Space: 56.7 MB (16.16%) - LeetHub

--- 2741-function-composition/2741-function-composition.js
+++ 2741-function-composition/2741-function-composition.js
@@ -3,10 +3,9 @@
  * @return {Function}
  */
 var compose = function (functions) {
-
-    return function (x) {
-        for (let i = functions.length - 1; i >= 0; i--) {
-            x = functions[i](x)
+    return function(x) {
+        for(let i=functions.length-1;i>=0;i--){
+            x=functions[i](x)
 
         }
         return x
