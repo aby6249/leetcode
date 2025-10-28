df1540711840bbc364345e583cfe7315049ed5fe
Aby Shaji <abyshaji318@gmail.com>
2025-10-28 10:51:11 +0530

    Time: 1 ms (43.55%), Space: 53.6 MB (82.52%) - LeetHub

--- 2507-number-of-common-factors/2507-number-of-common-factors.js
+++ 2507-number-of-common-factors/2507-number-of-common-factors.js
@@ -3,14 +3,14 @@
  * @param {number} b
  * @return {number}
  */
-var commonFactors = function(a, b) {
-     let count=0
-    for (let i=1;i<=a;i++){
-        if ( a%i==0 && b%i==0){
+var commonFactors = function (a, b) {
+    let count = 0
+    for (let i = 1; i <= a; i++) {
+        if (a % i == 0 && b % i == 0) {
             count++
 
         }
     }
-  return count
+    return count
 
 };
