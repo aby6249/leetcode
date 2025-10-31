36a5e6ae6f0f25af9ec0ff152c742973d8a72af5
Aby Shaji <abyshaji318@gmail.com>
2025-10-31 10:09:28 +0530

    Time: 0 ms (100%), Space: 57.2 MB (45.63%) - LeetHub

---
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+     let count=0
+    for (let i=0;i<nums.length;i++){
+        count += nums[i]
+        nums[i]=count
+
+    }
+    return nums
+};
