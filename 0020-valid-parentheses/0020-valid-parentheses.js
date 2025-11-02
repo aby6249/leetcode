ab5801a979827e67ca25d584fde559d0ca291ebf
Aby Shaji <abyshaji318@gmail.com>
2025-11-02 17:39:55 +0530

    Time: 0 ms (100%), Space: 57.3 MB (44.22%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,24 +2,24 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    const grill =[];
+var isValid = function (s) {
+    const grill = [];
 
-   for(let i=0; i<s.length;i++){
-    let x = s.charAt(i);
-    switch(x){
-        case '(': grill.push(')');
-        break;
-        case '{': grill.push('}');
-        break;
-        case '[': grill.push(']');
-        break;
-        default:
-        if(x !== grill.pop()){
-            return false
+    for (let i = 0; i < s.length; i++) {
+        let x = s.charAt(i);
+        switch (x) {
+            case '(': grill.push(')');
+                break;
+            case '{': grill.push('}');
+                break;
+            case '[': grill.push(']');
+                break;
+            default:
+                if (x !== grill.pop()) {
+                    return false
+                }
+
         }
-
     }
-   }
-   return grill.length === 0
+    return grill.length === 0
 };
