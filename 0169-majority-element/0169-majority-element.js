07c3d3316801c04afd2c6fe25f5819056960553d
Aby Shaji <abyshaji318@gmail.com>
2025-11-03 14:18:08 +0530

    Time: 1 ms (90.24%), Space: 57.3 MB (64.95%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let count=0
+    let x=[]
+    for(let i=0;i < nums.length;i++){
+        if(count === 0){
+            x = nums[i]
+            count =1
+        }else if (x === nums[i]){
+            count ++
+        }else{
+            count--
+        }
+    }
+    return x
+};
