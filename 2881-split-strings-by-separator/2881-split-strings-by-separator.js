1d7b1194fc72b37dfe0c3f891791ec0943d48fcc
Aby Shaji <abyshaji318@gmail.com>
2025-11-04 09:50:37 +0530

    Time: 8 ms (38.71%), Space: 61.2 MB (77.42%) - LeetHub

--- 2881-split-strings-by-separator/2881-split-strings-by-separator.js
+++ 2881-split-strings-by-separator/2881-split-strings-by-separator.js
@@ -3,13 +3,13 @@
  * @param {character} separator
  * @return {string[]}
  */
-var splitWordsBySeparator = function(words, separator) {
-    let arr=[]
+var splitWordsBySeparator = function (words, separator) {
+    let arr = []
 
-    for(let word of words){
-        let arr1= word.split(separator)
-        for(let grill of arr1){
-            if (grill !== ''){
+    for (let word of words) {
+        let arr1 = word.split(separator)
+        for (let grill of arr1) {
+            if (grill !== '') {
                 arr.push(grill)
 
             }
