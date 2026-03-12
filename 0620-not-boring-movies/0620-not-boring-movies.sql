48677af1127ff6522237b6c9d55a7d9989b36a20
Aby Shaji <abyshaji318@gmail.com>
2026-03-12 15:06:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,2 +1,1 @@
-# Write your MySQL query statement below
-select id,movie,description,rating from Cinema where description != 'boring' and id %2=1 order by rating desc 
+-- Write your PostgreSQL query statement below
